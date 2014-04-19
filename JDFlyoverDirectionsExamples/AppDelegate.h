323afbe31687a606e1dd892da3ac45263ce799ab
jcdouglas <jcdouglas6@gmail.com>
2014-04-19 16:34:54 -0500

    Initial Commit

---
+++ JDFlyoverDirectionsExamples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JDFlyoverDirectionsExamples
+//
+//  Created by Judson Douglas on 4/19/14.
+//  Copyright (c) 2014 Judson Douglas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JDFlyoverDirectionsExamples/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JDFlyoverDirectionsExamples
+//
+//  Created by Judson Douglas on 4/19/14.
+//  Copyright (c) 2014 Judson Douglas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
